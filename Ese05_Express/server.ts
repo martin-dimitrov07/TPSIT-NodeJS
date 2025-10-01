5d1a3fa38761dc8d1e2e73014ebfedac0fd142a4
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-10-01 12:51:04 +0200

    Finito Ese 05

--- Ese05_Express/server.ts
+++ Ese05_Express/server.ts
@@ -1,23 +1,21 @@
-// Importiamo le librerie http/url/fs (file system)
-// Consente di gestire le richieste HTTP
+//A. import delle librerie
 import http from "http";
-// Consente di gestire gli url/indirizzi
 import url from "url";
-// Consente di accedere al file system
-// javascript non accede al file system ma noi stiamo facendo un server quindi lato server si può accedere al file system
 import fs from "fs";
-
 import express from "express";
 
+//B. configurazioni
 const port: number = 1337;
 //riconosce i tipi automaticamente (non è any) -> grazie @types/node in devDependencies (sviluppo)
 const app = express();
-//stessa cosa
-//const app: express.Express = express();
+//stessa cosa -> const app: express.Express = express();
+
+
+//C. creazione ed avvio del server HTTP
 const server: http.Server = http.createServer(app);
 let paginaErr = "";
 
-
+//server in ascolto sulla porta 1337
 server.listen(port, function(){
     console.log("Server in ascolto sulla porta " + port);
 
@@ -27,4 +25,72 @@ server.listen(port, function(){
         else
             paginaErr = content.toString();
     })
+});
+
+//D. middleware
+//middleware 1: request log
+app.use(function(req, res, next) //se si omette => come risorsa "/"
+{
+    console.log("Ricevuta richiesta: " + req.method + ": " + req.originalUrl);
+    next(); //passa al middleware successivo
+});
+
+//middleware 2: gestione delle risorse statiche
+app.use(express.static("./static"));
+
+//middleware 3: gestione dei parametri post
+app.use(express.json({"limit": "5mb"})); //i parametri post sono restituiti in req.body
+//i parametri get invece sono restituiti come json in req.query
+
+//middleware 4: log dei parametri post
+app.use(function(req, res, next){
+    if(req.body && Object.keys(req.body).length > 0)
+        console.log("Parametri body: " + JSON.stringify(req.body));
+    next();
+});
+
+//E. gestione delle root dinamiche
+app.get("/api/richiesta1", function(req, res, next){
+    if(!req.query)
+        res.status(400).send("Parametri mancanti");
+    else
+        res.send(req.query);
+});
+
+app.patch("/api/richiesta2", function(req, res, next){
+    if(!req.body)
+        res.status(400).send("Parametri mancanti");
+    else
+        res.send(req.body);
+});
+
+//la tecnica del passaggio dei parametri all'interno della risorsa può essere usata con tutte le chiamate HTTP
+app.get("/api/richiestaParams/:gender/:id", function(req, res, next){
+    let gender = req.params.gender;
+    let id = req.params.id;
+
+    if(!gender || !id)
+    {
+        res.status(400).send("Parametri mancanti");
+    }
+    else
+    {
+        res.send({gender, id});
+    }
+});
+
+//F. default root e gestione errori
+app.use(function(req, res){
+    res.status(404);
+    
+    if(!req.originalUrl.startsWith("/api/"))
+        res.send(paginaErr);
+    else
+        res.send("Risorsa non trovata");
+});
+
+//G. gestione errori
+app.use(function(err: Error, req: express.Request, res: express.Response, next: express.NextFunction){
+    console.error("*** ERRORE ***:\n" + err.stack); //elenco completo degli errori
+    res.status(500).send("Errore interno del server");
 });

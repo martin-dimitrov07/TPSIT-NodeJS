9e7e047efe3b2b38b12ed52975400aa7ef02c7c2
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-10-01 11:08:19 +0200

    Inizio Ese 05

--- Ese05_Express/server.ts
+++ Ese05_Express/server.ts
@@ -7,4 +7,24 @@ import url from "url";
 // javascript non accede al file system ma noi stiamo facendo un server quindi lato server si può accedere al file system
 import fs from "fs";
 
-const port:number = 1337;
+import express from "express";
+
+const port: number = 1337;
+//riconosce i tipi automaticamente (non è any) -> grazie @types/node in devDependencies (sviluppo)
+const app = express();
+//stessa cosa
+//const app: express.Express = express();
+const server: http.Server = http.createServer(app);
+let paginaErr = "";
+
+
+server.listen(port, function(){
+    console.log("Server in ascolto sulla porta " + port);
+
+    fs.readFile("./static/error.html", function(err, content){ //content è una sequenza di byte
+        if(err)
+            paginaErr = "<h1>Risorsa non trovata</h1>";
+        else
+            paginaErr = content.toString();
+    })
+});
